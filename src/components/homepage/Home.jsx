2e94b79cdb29b5bf317f7f9531db0eab7f397ae7
devZahid <officialzahidhasan19932021@gmail.com>
2023-08-25 14:10:19 +0600

    adding services section to home page

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "./Banner";
+import Services from "./Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <div className="my-20">
+            <Services></Services>
+            </div>
         </div>
     );
 };
