874f3a3d1bba7a45fc8d9f6e9f8148350abf5d05
devZahid <officialzahidhasan19932021@gmail.com>
2023-08-31 14:33:25 +0600

    responsive add

--- src/components/homepage/CallUs.jsx
+++ src/components/homepage/CallUs.jsx
@@ -1,7 +1,7 @@
 
 const CallUs = () => {
     return (
-        <div className="text-gray-200 w-full bg-red-900 h-96 text-7xl text-center pt-[10rem] font-bold">
+        <div className="text-gray-200 w-full bg-red-900 h-96 text-3xl md:text-7xl text-center pt-[10rem] font-bold">
             <h1>+8801303289180</h1>
         </div>
     );
--- src/components/shared/SectionTitle.jsx
+++ src/components/shared/SectionTitle.jsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({ title }) => {
   return (
-    <div className="mt-10 mb-20 text-center text-4xl">
+    <div className="mt-10 mb-20 text-center text-2xl md:text-4xl">
       <h1 className="text-red-700">---{title}---</h1>
       <span className="inline-block bg-red-700 h-0.5 w-1/2"></span>
     </div>
