71bf18b91a57fe144138b99fc7ad7f6c9d2475c8
devZahid <officialzahidhasan19932021@gmail.com>
2023-09-02 18:36:30 +0600

    calling data to popular menu from  custoom hook

--- src/components/homepage/PopularMenu.jsx
+++ src/components/homepage/PopularMenu.jsx
@@ -1,27 +1,22 @@
 import { useEffect, useState } from "react";
 import FoodItems from "../shared/FoodItems";
+import useMenu from "../../customhooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItem = data.filter((item) => item.category === "popular");
-        setMenu(popularItem);
-      });
-  }, []);
+  const menu = useMenu();
+  const popularItem = menu.filter((item) => item.category === "popular");
 
   // console.log(menu);
   return (
     <div className="text-center">
       <div className="md:grid md:grid-cols-2 md:gap-20">
-        {menu.map((item) => (
+        {popularItem.map((item) => (
           <FoodItems key={item._id} item={item}></FoodItems>
         ))}
       </div>
-      <button className="mt-20 text-xl border-b-2 py-5 px-14 text-red-700 hover:bg-red-500 hover:text-white rounded-lg transition-colors duration-300 transform hover:scale-105">See More</button>
+      <button className="mt-20 text-xl border-b-2 py-5 px-14 text-red-700 hover:bg-red-500 hover:text-white rounded-lg transition-colors duration-300 transform hover:scale-105">
+        See More
+      </button>
     </div>
   );
 };
