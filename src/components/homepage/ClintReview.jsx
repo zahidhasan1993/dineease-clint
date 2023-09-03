32e622e23fb19150e1a6ae9c416fa427cf5b4543
devZahid <officialzahidhasan19932021@gmail.com>
2023-09-04 04:35:24 +0600

    adding server to the project

--- src/components/homepage/ClintReview.jsx
+++ src/components/homepage/ClintReview.jsx
@@ -10,7 +10,7 @@ const ClintReview = () => {
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
