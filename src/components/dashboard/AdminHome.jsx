e657e8bdaec5e738576d95d86ceda50bf1376f9c
devZahid <officialzahidhasan19932021@gmail.com>
2023-09-08 16:58:02 +0600

    adding some function and style to the admin dashboard

--- src/components/dashboard/AdminHome.jsx
+++ src/components/dashboard/AdminHome.jsx
@@ -1,10 +1,28 @@
-
+import { useContext } from "react";
+import { DataProvider } from "../../providers/AuthProvider";
+import useMenu from "../../customhooks/useMenu";
+import { PiForkKnifeBold } from "react-icons/pi";
 const AdminHome = () => {
-    return (
-        <div>
-            <h1 className="text-5xl">hello</h1>
+  const { user } = useContext(DataProvider);
+  const menu = useMenu();
+  // console.log(user);
+  return (
+    <div>
+      <h1 className="text-5xl">
+        Hi welcome back <span className="text-red-700">{user.displayName}</span>{" "}
+        !
+      </h1>
+      <div className="mt-10">
+        <div className="text-white bg-gradient-to-r from-red-700 to-pink-500 w-72 rounded-lg h-36 p-8 flex justify-center items-center gap-5">
+          <PiForkKnifeBold className="text-4xl"></PiForkKnifeBold>
+          <div className="text-2xl font-semibold">
+            <h1>{menu.length}</h1>
+            <p>Items</p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
