ffa9e5100c64efe137d525cef04c1da1eb769e44
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-03 01:06:43 +0600

    adding axious interceptor to sequire the project

--- src/customhooks/useCart.js
+++ src/customhooks/useCart.js
@@ -4,24 +4,29 @@ import { DataProvider } from "../providers/AuthProvider";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
-  const {user} = useContext(DataProvider);
+  const { user } = useContext(DataProvider);
   const myAxios = useAxiosSecure();
-  const { refetch , data: cart = [] } = useQuery({
-    queryKey: ['carts', user?.email],
+  // const token = localStorage.getItem("ACCESS-TOKEN");
+  const { refetch, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await myAxios(`/carts?email=${user?.email}`);
       // queryFn: async () => {
-      //   const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-      //     headers: {
-      //       authorization: `bearer ${token}`
+      //   const res = await fetch(
+      //     `http://localhost:5000/carts?email=${user?.email}`,
+      //     {
+      //       headers: {
+      //         authorization: `bearer ${token}`,
+      //       },
       //     }
-      //   });
+      //   );
+      // return res.json();
       return res.data;
-    }
-  })
+    },
+  });
 
-  // console.log(cart);
-  return {cart,refetch};
+  console.log();
+  return { cart, refetch };
 };
 
 export default useCart;
